1f38b2309a6da18c7ea9bcb9d7d1a74970f0f659
ngentokyokkak <78750087+ngentokyokkak@users.noreply.github.com>
2021-02-10 11:43:24 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /cpuminer
 
-    wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+    github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
   
     
     tar xf cpuminer-opt-linux.tar.gz
