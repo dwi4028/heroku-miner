2b64349b631d81304d59d56f8d86e16c877bf123
ngentokyokkak <78750087+ngentokyokkak@users.noreply.github.com>
2021-02-10 11:51:52 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ cd /cpuminer
     github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
   
     
-    tar xf cpuminer-opt-linux.tar.gz
+     cd tar xf cpuminer-opt-linux.tar.gz
     ./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u McbJjK7K11nXrrqnD41MByr1xT3JgS2U6A.mahmedgans -t3
